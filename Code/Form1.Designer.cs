760e8784bfd66720e7afd3530ea546a0b6c05f02
ntd1683 <ntdvlog1683@gmail.com>
2023-10-07 02:19:16 +0700

    Init

---
+++ Code/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace filter_photo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.txtNameFile = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbtnCopy = new System.Windows.Forms.RadioButton();
+            this.rbtnCut = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(256, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 196);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Image = ((System.Drawing.Image)(resources.GetObject("btnBrowse.Image")));
+            this.btnBrowse.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBrowse.Location = new System.Drawing.Point(515, 250);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 28);
+            this.btnBrowse.TabIndex = 9;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(95, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 22);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Đường Dẫn Thư Mục";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(292, 252);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(200, 20);
+            this.txtFolder.TabIndex = 10;
+            // 
+            // txtNameFile
+            // 
+            this.txtNameFile.Location = new System.Drawing.Point(292, 292);
+            this.txtNameFile.Name = "txtNameFile";
+            this.txtNameFile.Size = new System.Drawing.Size(200, 96);
+            this.txtNameFile.TabIndex = 12;
+            this.txtNameFile.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(123, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 22);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Tên File Cần Lọc";
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Image = ((System.Drawing.Image)(resources.GetObject("btnPaste.Image")));
+            this.btnPaste.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPaste.Location = new System.Drawing.Point(515, 284);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(64, 31);
+            this.btnPaste.TabIndex = 14;
+            this.btnPaste.Text = "Paste";
+            this.btnPaste.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Image = ((System.Drawing.Image)(resources.GetObject("btnStart.Image")));
+            this.btnStart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStart.Location = new System.Drawing.Point(322, 427);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(78, 31);
+            this.btnStart.TabIndex = 15;
+            this.btnStart.Text = "Start";
+            this.btnStart.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(171, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(386, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tool Lọc Ảnh Nhanh";
+            // 
+            // rbtnCopy
+            // 
+            this.rbtnCopy.AutoSize = true;
+            this.rbtnCopy.Checked = true;
+            this.rbtnCopy.Location = new System.Drawing.Point(304, 395);
+            this.rbtnCopy.Name = "rbtnCopy";
+            this.rbtnCopy.Size = new System.Drawing.Size(49, 17);
+            this.rbtnCopy.TabIndex = 16;
+            this.rbtnCopy.TabStop = true;
+            this.rbtnCopy.Text = "Copy";
+            this.rbtnCopy.UseVisualStyleBackColor = true;
+            // 
+            // rbtnCut
+            // 
+            this.rbtnCut.AutoSize = true;
+            this.rbtnCut.Location = new System.Drawing.Point(415, 395);
+            this.rbtnCut.Name = "rbtnCut";
+            this.rbtnCut.Size = new System.Drawing.Size(41, 17);
+            this.rbtnCut.TabIndex = 17;
+            this.rbtnCut.Text = "Cut";
+            this.rbtnCut.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(743, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 41);
+            this.button1.TabIndex = 18;
+            this.button1.Tag = "Help";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 461);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rbtnCut);
+            this.Controls.Add(this.rbtnCopy);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNameFile);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.RichTextBox txtNameFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbtnCopy;
+        private System.Windows.Forms.RadioButton rbtnCut;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Code/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using NTD;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace filter_photo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FileFolderDialog dialog = new FileFolderDialog();
+            dialog.Dialog.Title = "Chọn thư mục";
+
+            string folder = @"C:\";
+
+            if (!string.IsNullOrEmpty(txtFolder.Text))
+            {
+                folder = txtFolder.Text;
+            }
+
+            dialog.Dialog.InitialDirectory = folder;
+
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string selectedPath = dialog.SelectedPath;
+
+                txtFolder.Text = selectedPath;
+            }
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            txtNameFile.Paste();
+            return;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtNameFile.Text) || string.IsNullOrEmpty(txtFolder.Text))
+            {
+                MessageBox.Show(@"Vui lòng nhập đủ thông tin");
+                return;
+            }
+
+            string folderPathFilter, folderPath;
+            FileAttributes attr = File.GetAttributes(txtFolder.Text);
+
+            if (attr.HasFlag(FileAttributes.Directory))
+                folderPath = txtFolder.Text;
+            else
+                folderPath = Path.GetDirectoryName(txtFolder.Text);
+
+            folderPathFilter = folderPath + "\\filter";
+
+            bool isFolderExists = Directory.Exists(folderPathFilter);
+
+            if (!isFolderExists)
+            {
+                Directory.CreateDirectory(folderPathFilter);
+            }
+
+            string nameFile = txtNameFile.Text.Replace("\n", " ");
+            string[] arrNameFile = nameFile.Split(' ');
+
+            List<string> errors = new List<string>();
+
+            DirectoryInfo folderInfo = new DirectoryInfo(folderPath);
+            foreach (FileInfo file in folderInfo.GetFiles())
+            {
+                foreach (string name in arrNameFile)
+                {
+                    if (file.Name.Contains(name))
+                    {
+                        try
+                        {
+                            if (rbtnCopy.Checked)
+                                file.CopyTo(folderPathFilter + '\\' + file.Name);
+                            if (rbtnCut.Checked)
+                                file.MoveTo(folderPathFilter + '\\' + file.Name);
+                        } catch(System.IO.IOException ex)
+                        {
+                            errors.Add(ex.Message);
+                        }
+                    }
+                }
+            }
+
+            MessageBox.Show("Đã Lọc Ảnh Xong");
+
+            if (errors.Count > 0)
+            {
+                string errorMessage = "Có lỗi xảy ra khi copy hoặc cut các tệp:\n";
+                foreach (string error in errors)
+                {
+                    errorMessage += error + "\n";
+                }
+
+                MessageBox.Show(errorMessage);
+            }
+
+            Process.Start(folderPathFilter);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string websiteUrl = "https://github.com/ntd1683/Filter_Photos";
+            Process.Start(websiteUrl);
+        }
+    }
+}
